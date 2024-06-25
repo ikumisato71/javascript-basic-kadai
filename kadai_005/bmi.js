f7790b92f17808266bb41174c659dcc9fbb2c80d
ikumisato <kinoko7971@gmail.com>
2024-06-26 05:15:16 +0900

    005 007 009の解答を追加

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -7,3 +7,8 @@ height = 1.7;
 let bmi = bodyWeight / (height * height);
 
 console.log(bmi);
+
+// let weight = 68;
+// let height = 1.7;
+// let bmi = weight / (height * height);
+// console.log(bmi);
