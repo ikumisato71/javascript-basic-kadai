612d9a3cf9e717768ef696401488098e34b0911e
ikumisato <kinoko7971@gmail.com>
2024-06-26 09:05:28 +0900

    コメントアウトの付け方を試す

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -7,8 +7,9 @@ height = 1.7;
 let bmi = bodyWeight / (height * height);
 
 console.log(bmi);
-
-// let weight = 68;
-// let height = 1.7;
-// let bmi = weight / (height * height);
-// console.log(bmi);
+/*
+let weight = 68;
+let height = 1.7;
+ let bmi = weight / (height * height);
+console.log(bmi);
+*/
