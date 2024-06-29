ec41f8275025efd7d3568e565b777a3bea1b48b2
ikumisato <kinoko7971@gmail.com>
2024-06-29 11:58:31 +0900

    確認打ち込み

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -24,3 +24,15 @@ if (num % 3 === 0 && num % 5 === 0) {
 // else {
 //     console.log(num);
 // }
+
+// let num = Math.floor(Math.random() * 20) + 1;
+// console.log(num);
+// if (num % 3 === 0 && num % 5 === 0) {
+//   console.log("3と5の倍数です");
+// } else if (num % 3 === 0) {
+//   console.log("3の倍数です");
+// } else if (num % 5 === 0) {
+//   console.log("5の倍数です");
+// } else {
+//   console.log(num);
+// }
