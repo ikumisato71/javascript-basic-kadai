1d5e1f6fd85deb050adc3dd9fec4259ce420fdd7
ikumisato <kinoko7971@gmail.com>
2024-07-04 05:06:58 +0900

    kadai_020を修正

--- kadai_020/event.js
+++ kadai_020/event.js
@@ -1,8 +1,6 @@
 const button = document.getElementById("btn");
 const text = document.getElementById("text");
-const clickedText = document.getElementById("text-click");
 
 button.addEventListener("click", () => {
-  text.style.display = "none";
-  clickedText.style.display = "block";
+  text.textContent = "ボタンをクリックしました";
 });
